e874d8fa5b7f5463d1df6a2eb16d15e8a9b0be47
ddovguchev <ddovguchev@gmail.com>
2026-06-22 08:27:02 +0300

    init

--- system-module-configuration/programs.nix
+++ system-module-configuration/programs.nix
@@ -1,6 +1,10 @@
 programs:
-{ lib, ... }: {
-  programs.wireguard.enable = lib.mkIf (programs.wireguard or false) true;
+{ lib, pkgs, ... }: {
+  networking.wireguard.enable = lib.mkIf (programs.wireguard or false) true;
+
+  environment.systemPackages = lib.mkIf (programs.wireguard or false) [
+    pkgs.wireguard-tools
+  ];
 
   programs.steam = lib.mkIf (programs.steam or false) {
     enable = true;
